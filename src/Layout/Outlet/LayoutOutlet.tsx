6b31cca52c5b83202f9b74c39d7ff85716af4a52
João Vitor Rosa <joao4900@outlook.com>
2026-05-29 13:16:02 -0300

    resolução de bug em feed

--- src/Layout/Outlet/LayoutOutlet.tsx
+++ src/Layout/Outlet/LayoutOutlet.tsx
@@ -3,17 +3,20 @@ import * as S from "./styleOutlet";
 import { Menu } from "../../components/Menu/Menu.tsx";
 import { Trends } from "../../components/Trends/Trends.tsx";
 import { UserListProvider } from "../../context/UserListContext.tsx";
+import { TweetProvider } from "../../context/TweetContext.tsx";
 
 export function LayoutOutlet() {
     return (
-        <UserListProvider>
-            <S.Wrapper>
-                <Menu/>
-                <S.Main>
-                    <Outlet/>
-                </S.Main>
-                <Trends/>
-            </S.Wrapper>
-        </UserListProvider>
+        <TweetProvider>
+            <UserListProvider>
+                <S.Wrapper>
+                    <Menu/>
+                    <S.Main>
+                        <Outlet/>
+                    </S.Main>
+                    <Trends/>
+                </S.Wrapper>
+            </UserListProvider>
+        </TweetProvider>
     )
 }
--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,11 +1,23 @@
-
+import { useEffect } from "react";
+import { TweetCard } from "../../components/Tweets/TweetCards";
+import { useTweet } from "../../hooks/useTweet";
 import * as S from "./stylesFeed";
 
 export function Feed() {
-    
+    const { tweets, isLoading, refreshFeed } = useTweet();
+
+    useEffect(() => {
+        refreshFeed();
+    }, [refreshFeed]);
+
+    if (isLoading) return <p>Carregando...</p>;
+
     return (
         <S.Container>
             <S.PageTitle>Página Inicial</S.PageTitle>
+            {tweets.map((tweet) => (
+                <TweetCard key={tweet.id} tweet={tweet} />
+            ))}
         </S.Container>
     );
 }
